93fa4ab6b9efbaa184a9f72b143605b195334ad4
namgivu <namgivu@gmail.com>
2017-11-16 15:29:03 +0700

    more empty array check

--- empty-array.php
+++ empty-array.php
@@ -1,25 +1,23 @@
 <?php
 
-$a=[]; echo getEmptyCheckedValue('plain', $a), PHP_EOL;
-$a=[null]; echo getEmptyCheckedValue('plain', $a), PHP_EOL;
-$a=[[null]]; echo getEmptyCheckedValue('plain', $a), PHP_EOL;
+$a=[]; echo getEmptyCheckedValue('isEmpty', $a), PHP_EOL;
+$a=[null]; echo getEmptyCheckedValue('isEmpty', $a), PHP_EOL;
+$a=[[null]]; echo getEmptyCheckedValue('isEmpty', $a), PHP_EOL;
 
-
-$a=[null]; $b=array_filter($a); echo getEmptyCheckedValue('array_filter', $b), PHP_EOL;
-$a=[[null]]; $b=array_filter($a); echo getEmptyCheckedValue('array_filter', $b), PHP_EOL;
-
-
-$a=[[null]]; $c=implode($a); echo getEmptyCheckedValue('implode', $c),      ' '.$c.' strlen='.strlen($c).PHP_EOL;
-$a=[1, 2];   $c=implode($a); echo getEmptyCheckedValue('implode', $c),      ' '.$c.' strlen='.strlen($c).PHP_EOL;
+$a=[[null]]; $c=implode($a); echo $c.PHP_EOL;
+$a=[1, 2];   $c=implode($a); echo $c.PHP_EOL;
 
 
 function getEmptyCheckedValue($label, $a) {
     if (empty($a)) {
         $b='1 empty';
+    } else if (!array_filter($a)) {
+        $b='1b empty';
     } else if ($a==[[null]]) {
         $b=2;
     } else {
         $b=0;
     }
-    return "$label | $b";
+    var_dump($a);
+    return "$label | $b".PHP_EOL;
 }
---
+++ empty-array2.php
@@ -0,0 +1,8 @@
+<?php
+
+$a=[]; $b=(array_filter($a))? implode(',',$a) : 'empty'; echo $b.PHP_EOL;
+$a=[null]; $b=(array_filter($a))? implode(',',$a) : 'empty';  echo $b.PHP_EOL;
+$a=[null,null]; $b=(array_filter($a))? implode(',',$a) : 'empty';  echo $b.PHP_EOL;
+$a=[null,1,null]; $b=(array_filter($a))? implode(',',$a) : 'empty';  echo $b.PHP_EOL;
+$a=[1]; $b=(array_filter($a))? implode(',',$a) : 'empty';  echo $b.PHP_EOL;
+$a=[1,2]; $b=(array_filter($a))? implode(',',$a) : 'empty';  echo $b.PHP_EOL;
