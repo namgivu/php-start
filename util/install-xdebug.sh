b50d427860085196920d5ff58bfe19619d6c14f3
namgivu <namgivu@gmail.com>
2017-12-13 14:30:20 +0700

    rename util

--- util/install-xdebug.sh
+++ util/install-xdebug.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+: #we need xdebug installed to have PhpStorm debug work
+
 #install xdebug ref. http://www.dieuwe.com/blog/xdebug-ubuntu-1604-php7
 sudo apt-get install php-xdebug && sudo service apache2 restart
 php -m | grep xdebug #aftermath check
