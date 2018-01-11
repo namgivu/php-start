6652a16b870e1f5c19eb0e4736a62e797ae13883
namgivu <namgivu@gmail.com>
2018-01-11 08:19:45 +0700

    datetime more fun

--- sandbox/datetime.php
+++ sandbox/datetime.php
@@ -4,7 +4,16 @@
 $now=new DateTime();
 
 #add 1 week ref. https://stackoverflow.com/a/15859078/248616
-$d=$now;
+$d=$now;       #caution, this will take reference of $now to $d, not a deep copy
+$d=clone $now; #this is the right deep copy ref. https://stackoverflow.com/a/2579471/248616
 echo $d->format('Y-m-d H:i:s').PHP_EOL;
 $d->modify('+1 week');
 echo $d->format('Y-m-d H:i:s').PHP_EOL;
+
+#compare ref. https://stackoverflow.com/a/16814230/248616
+echo PHP_EOL;
+echo $now->format('Y-m-d H:i:s').PHP_EOL;
+echo $d  ->format('Y-m-d H:i:s').PHP_EOL;
+var_dump($now < $d);
+var_dump($now > $d);
+var_dump($now == $d);
